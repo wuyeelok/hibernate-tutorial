c9eadf224d5f45d94e16e3689e0b6c806816983e
wuyeelok <wuyeelok@gmail.com>
2019-08-08 17:32:24 +0800

    Create starter file for update student

---
+++ src/com/luv2code/hibernate/demo/UpdateStudentDemo.java
@@ -0,0 +1,48 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class UpdateStudentDemo {
+
+	public static void main(String[] args) {
+
+		// Create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		// Create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			int studentId = 1;
+
+			// Start a transaction
+			session.beginTransaction();
+
+			// retrieve student based on the id: primary key
+			System.out.println("\n\nGetting student with id: " + studentId);
+
+			Student myStudent = session.get(Student.class, studentId);
+			System.out.println("\n\nMy student: " + myStudent);
+
+			// commit the transaction
+			session.getTransaction().commit();
+
+			System.out.println("DONE!");
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (factory != null) {
+				factory.close();
+			}
+		}
+
+	}
+
+}
