1012de1b4a8afedbce7c07fc1e0f9b9210856b14
wuyeelok <wuyeelok@gmail.com>
2019-08-09 12:25:33 +0800

    Update all student's email

--- src/com/luv2code/hibernate/demo/UpdateStudentDemo.java
+++ src/com/luv2code/hibernate/demo/UpdateStudentDemo.java
@@ -1,5 +1,8 @@
 package com.luv2code.hibernate.demo;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaUpdate;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -11,13 +14,16 @@ public class UpdateStudentDemo {
 	public static void main(String[] args) {
 
 		// Create session factory
-		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
-				.buildSessionFactory();
+		SessionFactory factory = null;
 
-		// Create session
-		Session session = factory.getCurrentSession();
-
+		Session session = null;
 		try {
+			// Create session factory
+			factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+					.buildSessionFactory();
+
+			// Create session
+			session = factory.getCurrentSession();
 
 			int studentId = 1;
 
@@ -35,6 +41,27 @@ public class UpdateStudentDemo {
 
 			// commit the transaction
 			session.getTransaction().commit();
+
+			// New Code
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+
+			// Update email for all students.
+			System.out.println("Update email for all students");
+			CriteriaBuilder cb = session.getCriteriaBuilder();
+			CriteriaUpdate<Student> cu = cb.createCriteriaUpdate(Student.class);
+
+			cu.from(Student.class);
+			cu.set("email", "wuyeelok@gmail.com");
+
+			int affected = session.createQuery(cu).executeUpdate();
+			System.out.println("Affected row: " + affected);
+
+			// Commit the transaction
+			session.getTransaction().commit();
+			if (session != null) {
+				session.close();
+			}
 
 			System.out.println("DONE!");
 
