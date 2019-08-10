787db33d3da04ca20c131cf68286829219cfa1a1
wuyeelok <wuyeelok@gmail.com>
2019-08-10 20:39:48 +0800

    Delete single student using session

---
+++ src/com/luv2code/hibernate/demo/DeleteStudentDemo.java
@@ -0,0 +1,63 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class DeleteStudentDemo {
+
+	public static void main(String[] args) {
+
+		// Create session factory
+		SessionFactory factory = null;
+
+		Session session = null;
+		try {
+			// Create session factory
+			factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+					.buildSessionFactory();
+
+			// Create session
+			session = factory.getCurrentSession();
+
+			int studentId = 1;
+
+			// Start a transaction
+			session.beginTransaction();
+
+			// retrieve student based on the id: primary key
+			System.out.println("\n\nGetting student with id: " + studentId);
+
+			Student myStudent = session.get(Student.class, studentId);
+			System.out.println("\n\nMy student: " + myStudent);
+
+			if (myStudent != null) {
+				// Delete the student
+				System.out.println("Deleting student: " + myStudent);
+				session.delete(myStudent);
+			} else {
+				System.out.println("Can not find student with id: " + studentId);
+			}
+
+			// commit the transaction
+			session.getTransaction().commit();
+
+			if (session != null) {
+				session.close();
+			}
+
+			System.out.println("DONE!");
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (factory != null) {
+				factory.close();
+			}
+		}
+
+	}
+
+}
