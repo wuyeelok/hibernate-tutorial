62e5f302d4666cf053c104cfdb42678a054a90b2
wuyeelok <wuyeelok@gmail.com>
2019-08-15 09:40:33 +0800

    Set up session factory for instructor

--- src/com/luv2code/hibernate/demo/CreateDemo.java
+++ src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -1,39 +1,27 @@
 package com.luv2code.hibernate.demo;
 
-import java.util.Date;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import com.luv2code.hibernate.demo.entity.Student;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
 
 public class CreateDemo {
 
 	public static void main(String[] args) {
 
 		// Create session factory
-		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
-				.buildSessionFactory();
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
 
 		// Create session
 		Session session = factory.getCurrentSession();
 
 		try {
-			// Create a student object
-			System.out.println("Creating new student object...");
-
-			String theDateOfBirthStr = "23/12/1995";
-			Date theDateOfBirth = DateUtils.parseDate(theDateOfBirthStr);
-
-			Student tempStudent = new Student("Paul", "Krugman", theDateOfBirth, "paul@luv2.code.com");
 
 			// Start a transaction
 			session.beginTransaction();
-
-			// Save the student object
-			System.out.println("Saving the student");
-			session.save(tempStudent);
 
 			// Commit transaction
 			session.getTransaction().commit();
