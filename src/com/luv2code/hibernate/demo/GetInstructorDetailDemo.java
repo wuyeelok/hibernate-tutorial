2078eb87bd476cec305d923aabb8dc9131217e3e
wuyeelok <wuyeelok@gmail.com>
2019-08-18 16:51:11 +0800

    Rename typo class

--- src/com/luv2code/hibernate/demo/GetInstructorDetailDemo.java
+++ src/com/luv2code/hibernate/demo/GetInstructorDetailDemo.java
@@ -7,7 +7,7 @@ import org.hibernate.cfg.Configuration;
 import com.luv2code.hibernate.demo.entity.Instructor;
 import com.luv2code.hibernate.demo.entity.InstructorDetail;
 
-public class GetInstructorDeatilDemo {
+public class GetInstructorDetailDemo {
 
 	public static void main(String[] args) {
 
