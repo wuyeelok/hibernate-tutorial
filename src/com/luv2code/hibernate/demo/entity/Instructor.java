c06ea10d123ace03c66c8dc30eb76ef48f778f8c
wuyeelok <wuyeelok@gmail.com>
2019-08-25 17:50:12 +0800

    Lazy fetch Instructor & related course

--- src/com/luv2code/hibernate/demo/entity/Instructor.java
+++ src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -47,7 +47,7 @@ public class Instructor {
 	@JoinColumn(name = "instructor_detail_id")
 	private InstructorDetail instructorDetail;
 
-	@OneToMany(fetch = FetchType.EAGER, mappedBy = "instructor", cascade = { CascadeType.PERSIST, CascadeType.MERGE,
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "instructor", cascade = { CascadeType.PERSIST, CascadeType.MERGE,
 			CascadeType.DETACH, CascadeType.REFRESH })
 	private List<Course> courses;
 
