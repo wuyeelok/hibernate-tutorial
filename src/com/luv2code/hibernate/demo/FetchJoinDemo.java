0572f91612e44c18ce251dc7df58f945f4745448
wuyeelok <wuyeelok@gmail.com>
2019-08-26 16:12:17 +0800

    Fetch join demo starter file

---
+++ src/com/luv2code/hibernate/demo/FetchJoinDemo.java
@@ -0,0 +1,58 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class FetchJoinDemo {
+
+	public static void main(String[] args) {
+
+		// Create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class).buildSessionFactory();
+
+		// Create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// Start a transaction
+			session.beginTransaction();
+
+			// Get the instructor from DB
+			int theId = 1;
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+
+			System.out.println("luv2code: Instructor: " + tempInstructor);
+
+			// Commit transaction
+			session.getTransaction().commit();
+
+			// Close the session
+			session.close();
+
+			// Since courses are lazy loaded ... this should fail
+			System.out.println("\nluv2code: The session is now closed!\n");
+			// Solution 1: Call getter method while session is open
+
+			// Get course for the instructor
+			System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
+
+			System.out.println("luv2code: DONE!");
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (factory != null) {
+				factory.close();
+			}
+		}
+
+	}
+
+}
