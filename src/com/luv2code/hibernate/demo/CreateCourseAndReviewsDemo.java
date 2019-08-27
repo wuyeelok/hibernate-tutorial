fd62bd23b5333398d5d2c047f494de31a0e3dc60
wuyeelok <wuyeelok@gmail.com>
2019-08-27 10:58:23 +0800

    Create Course And Reviews starter file

---
+++ src/com/luv2code/hibernate/demo/CreateCourseAndReviewsDemo.java
@@ -0,0 +1,42 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class CreateCourseAndReviewsDemo {
+
+	public static void main(String[] args) {
+
+		// Create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class).buildSessionFactory();
+
+		// Create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// Start a transaction
+			session.beginTransaction();
+
+			// Commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("DONE!");
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (factory != null) {
+				factory.close();
+			}
+		}
+
+	}
+
+}
