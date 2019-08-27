a8d3ae61f34b9e9851f25b9d6fd8938cef6ea712
wuyeelok <wuyeelok@gmail.com>
2019-08-27 12:16:00 +0800

    Create Course & related Review in one go

--- src/com/luv2code/hibernate/demo/CreateCourseAndReviewsDemo.java
+++ src/com/luv2code/hibernate/demo/CreateCourseAndReviewsDemo.java
@@ -7,6 +7,7 @@ import org.hibernate.cfg.Configuration;
 import com.luv2code.hibernate.demo.entity.Course;
 import com.luv2code.hibernate.demo.entity.Instructor;
 import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
 
 public class CreateCourseAndReviewsDemo {
 
@@ -14,7 +15,8 @@ public class CreateCourseAndReviewsDemo {
 
 		// Create session factory
 		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
-				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class).buildSessionFactory();
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class).buildSessionFactory();
 
 		// Create session
 		Session session = factory.getCurrentSession();
@@ -23,6 +25,20 @@ public class CreateCourseAndReviewsDemo {
 
 			// Start a transaction
 			session.beginTransaction();
+
+			// Create a course
+			Course tempCourse = new Course("Pacman - How To Score One Million Points");
+
+			// Add some reviews
+			tempCourse.addReview(new Review("Greate course ... loved it!"));
+			tempCourse.addReview(new Review("Cool course, job well done"));
+			tempCourse.addReview(new Review("What a dump course, you are an idiot!"));
+
+			// Save the course ... and leverage the cascade all
+			System.out.println("Saving the course");
+			System.out.println(tempCourse);
+			System.out.println(tempCourse.getReviews());
+			session.save(tempCourse);
 
 			// Commit transaction
 			session.getTransaction().commit();
