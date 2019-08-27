8ced04cb620d1f002a8165b352fc0f73a9127566
wuyeelok <wuyeelok@gmail.com>
2019-08-27 16:43:41 +0800

    Add student list in course class

--- src/com/luv2code/hibernate/demo/entity/Course.java
+++ src/com/luv2code/hibernate/demo/entity/Course.java
@@ -45,6 +45,8 @@ public class Course {
 	@JoinColumn(name = "course_id")
 	private List<Review> reviews;
 
+	private List<Student> students;
+
 	public Course() {
 
 	}
@@ -93,6 +95,24 @@ public class Course {
 		}
 
 		reviews.add(theReview);
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	// Add a convenience method
+	public void addStudent(Student theStudent) {
+
+		if (students == null) {
+			students = new ArrayList<>();
+		}
+
+		students.add(theStudent);
 	}
 
 	@Override
