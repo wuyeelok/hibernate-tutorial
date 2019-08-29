71120ec0a927a85fa1b7c386a043fe39f1c4ab6c
wuyeelok <wuyeelok@gmail.com>
2019-08-29 10:11:38 +0800

    DeleteMaryStudentDemo starter

---
+++ src/com/luv2code/hibernate/demo/DeleteMaryStudentDemo.java
@@ -0,0 +1,52 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class DeleteMaryStudentDemo {
+
+	public static void main(String[] args) {
+
+		// Create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class).addAnnotatedClass(Student.class).buildSessionFactory();
+
+		// Create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// Start a transaction
+			session.beginTransaction();
+
+			// Get the student Mary from database
+			int studentId = 2;
+			Student tempStudent = session.get(Student.class, studentId);
+
+			System.out.println("\n\nLoaded student: " + tempStudent);
+			System.out.println("\nCourses: " + tempStudent.getCourses());
+
+			// Commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("DONE!");
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (factory != null) {
+				factory.close();
+			}
+		}
+
+	}
+
+}
